daa68891315f37b74d7ac0e61bede78e40bba2dc
jane096 <jes7077@naver.com>
2024-02-25 00:11:58 +0900

    refactor: 보너스볼 판별여부 수정

--- src/main/java/refactoring/enumeration/LottoRank.java
+++ src/main/java/refactoring/enumeration/LottoRank.java
@@ -4,26 +4,27 @@ import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public enum LottoRank {
 
-    FIRST(6, 2_000_000_000, false, "6개 일치 (2000000000원) - %s개"),
-    SECOND(5, 30_000_000, true, "5개 일치, 보너스 볼 일치 (30000000원) - %s개"),
-    THIRD(5, 1_500_000, false, "5개 일치 (1500000원) - %s개"),
-    FOURTH(4, 50_000, false, "4개 일치 (50000원) - %s개"),
-    FIFTH(3, 5_000, false, "3개 일치 (5000원) - %s개"),
-    ZERO(0, 0, false, "");
+    FIRST(6, 2_000_000_000, (match, isBonus) -> match == 6, "6개 일치 (2000000000원) - %s개"),
+    SECOND(5, 30_000_000, (match, isBonus) -> match == 5 && isBonus, "5개 일치, 보너스 볼 일치 (30000000원) - %s개"),
+    THIRD(5, 1_500_000, (match, isBonus) -> match == 5 && !isBonus, "5개 일치 (1500000원) - %s개"),
+    FOURTH(4, 50_000, (match, isBonus) -> match == 4, "4개 일치 (50000원) - %s개"),
+    FIFTH(3, 5_000, (match, isBonus) -> match == 3, "3개 일치 (5000원) - %s개"),
+    ZERO(0, 0, (match, isBonus) -> match < 3  , "");
 
     private final int match;
     private final int prize;
-    private final boolean isBonus;
+    private final BiFunction<Integer, Boolean, Boolean> condition;
     private final String message;
 
-    LottoRank(int match, int prize, boolean isBonus, String message) {
+    LottoRank(int match, int prize, BiFunction<Integer, Boolean, Boolean> condition, String message) {
         this.match = match;
         this.prize = prize;
-        this.isBonus = isBonus;
+        this.condition = condition;
         this.message = message;
     }
 
@@ -38,14 +39,14 @@ public enum LottoRank {
     }
 
     public static LottoRank getRank(int match, boolean isBonus) {
-        if (match == THIRD.match && isBonus) {
-            return SECOND;
-        }
-
         return ALL_LOTTO_RANK.stream()
-                .filter(lottoRank -> lottoRank.match == match)
+                .filter(lottoRank -> lottoRank.condition(match, isBonus))
                 .findAny()
                 .orElse(ZERO);
+    }
+
+    private boolean condition(int match, boolean isBonus) {
+        return this.condition.apply(match, isBonus);
     }
 
     public static Map<LottoRank, Integer> initialize() {
