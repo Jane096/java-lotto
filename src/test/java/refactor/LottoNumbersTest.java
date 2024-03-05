7975d131cd600bded5e99abc60bef5b5e93b31bb
jane096 <jes7077@naver.com>
2024-03-05 23:47:26 +0900

    refactor: 테스트는 1개만 하도록 수정, BonusNumber 클래스 분리로 테스트 삭제

--- src/test/java/refactor/LottoNumbersTest.java
+++ src/test/java/refactor/LottoNumbersTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-import refactoring.model.BonusNumber;
 import refactoring.model.LottoNumber;
 import refactoring.model.LottoNumbers;
 
@@ -43,17 +41,6 @@ public class LottoNumbersTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1:false","21:true","23:false","14:true","24:false"}, delimiter = ':')
-    @DisplayName("로또 번호 중에서 보너스 번호와 일치하는 숫자가 있다면 리턴한다.")
-    void isBonusMatchedTest(int bonus, boolean expectedResult) {
-        Set<LottoNumber> lottoNumberTest = Set.of(LottoNumber.of(14),LottoNumber.of(21),LottoNumber.of(32),LottoNumber.of(41),LottoNumber.of(25),LottoNumber.of(5));
-        LottoNumbers lottoNumbers = LottoNumbers.of(lottoNumberTest);
-
-        boolean isBonusMated = new BonusNumber(bonus).isBonusMatched(lottoNumbers.getNumbers());
-        assertThat(isBonusMated).isEqualTo(expectedResult);
-    }
-
-    @ParameterizedTest
     @MethodSource("generateData")
     @DisplayName("로또숫자가 6개 이상이라면 예외를 발생시키지 않는다.")
     void validateTest(Set<LottoNumber> numbers) {
@@ -62,13 +49,17 @@ public class LottoNumbersTest {
     }
 
     @Test
-    @DisplayName("로또숫자가 6개 이하라면 예외를 발생시킨다.")
-    void validateExceptionTest() {
+    @DisplayName("로또숫자가 6개 이하라면 예외를 발생시킨다. (5개일 때)")
+    void validateExceptionFiveNumbersTest() {
         Set<LottoNumber> lottoNumberTest = Set.of(LottoNumber.of(14),LottoNumber.of(21),LottoNumber.of(32),LottoNumber.of(41),LottoNumber.of(25));
         assertThatThrownBy(() -> LottoNumbers.of(lottoNumberTest))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("로또 숫자는 6개이어야 합니다.");
+    }
 
+    @Test
+    @DisplayName("로또숫자가 6개 이하라면 예외를 발생시킨다. (4개일 때)")
+    void validateExceptionFourNumbersTest() {
         Set<LottoNumber> lottoNumberTestSecond = Set.of(LottoNumber.of(14),LottoNumber.of(21),LottoNumber.of(32),LottoNumber.of(41));
         assertThatThrownBy(() -> LottoNumbers.of(lottoNumberTestSecond))
                 .isInstanceOf(IllegalArgumentException.class)
